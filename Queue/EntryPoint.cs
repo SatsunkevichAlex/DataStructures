2a98db1554b1c02a49a0ad05ebaad9b16e302796
Alex <sasha.satsunkevich@gmail.com>
2019-01-29 22:06:10 +0300

    add sample with queue

---
+++ Queue/EntryPoint.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Queue
+{
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> queue = new Queue<string>();
+            queue.Enqueue("Kate");
+            queue.Enqueue("Sam");
+            queue.Enqueue("Alice");
+            queue.Enqueue("Tom");
+
+            foreach (string item in queue)
+                Console.WriteLine(item);
+            Console.WriteLine();
+
+            string firstItem = queue.Dequeue();
+            Console.WriteLine($"Extracted item: {firstItem}");
+            Console.WriteLine();
+
+            foreach (string item in queue)
+                Console.WriteLine(item);
+        }
+    }
+}
